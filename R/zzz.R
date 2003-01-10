ff1a5ad3d252e7511fce8223d7420d2d4813c32c
jhnzhang <unknown>
2003-01-10 22:12:51 +0000

    zzz.R modified to require tkWidgets.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@2975 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
-# This function initializes the classes needed
+# This function loads the required libraries and initializes the classes
 
 .First.lib <- function(libname, pkgname, where) {
-    require(methods, quietly=TRUE)
-    require(tcltk, quietly = TRUE)
-    require(tkWidgets, quietly = TRUE)
+    require(methods, quietly=TRUE) ||
+                                 stop("Package methods unavailable!")
+    require(tkWidgets, quietly = TRUE) ||
+                                 stop("Package tkWidgets unavailable!")
 
     where <- match(paste("package:", pkgname, sep=""), search())
     .initBasicPW(where)
