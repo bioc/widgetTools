2fb914587e71dc80d34ba5e03c9f57b9db232899
jhnzhang <unknown>
2003-01-28 15:39:38 +0000

    Checking added to take care of the display problem.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@3169 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,15 @@
 .First.lib <- function(libname, pkgname, where) {
     require(methods, quietly=TRUE) ||
                                  stop("Package methods unavailable!")
-    require(tcltk, quietly = TRUE) ||
+    capable <- capabilities()
+    if(!capable["tcltk"]){
+        warning("The tcl/tk library is not available")
+    }else{
+        if(interactive()){
+            require(tcltk, quietly = TRUE) ||
                                  stop("Package tcltk unavailable!")
+        }
+    }
 
     where <- match(paste("package:", pkgname, sep=""), search())
     .initBasicPW(where)
