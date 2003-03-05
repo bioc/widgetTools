99c83f5fd37740a88461f95713a084a89b49ff76
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-03-05 16:06:22 +0000

    test
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@3501 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -8,14 +8,22 @@
         warning("The tcl/tk library is not available")
     }else{
         if(interactive()){
+            out <- paste("Package tcltk not able to be loaded!")
+            if (.Platform$OS.type == "windows")
+                out <- paste(out,"\nThe most likely cause of this",
+                             "is that your Tcl/Tk installation is",
+                             "misconfigured\nPlease see the R",
+                             "Windows FAQ, question 3.6:\n",
+                             "http://cran.r-project.org/bin/windows/contrib/rw-FAQ.html#Package%20TclTk%20does%20not%20work.")
+
             require(tcltk, quietly = TRUE) ||
-                                 stop("Package tcltk unavailable!")
+                                 stop(out)
         }
     }
 
     if(require(Biobase)){
         if(interactive()){
-            if(require(tcltk, quietly = TRUE)){ 
+            if(require(tcltk, quietly = TRUE)){
                 addVig2Menu("widgetTools")
             }
         }
