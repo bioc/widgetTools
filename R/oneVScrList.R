a28bcf4831e1e32e8aefeaf9b38ecdf715a55523
jhnzhang <unknown>
2003-10-01 19:03:59 +0000

    A new widget added. JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@5872 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/oneVScrList.R
@@ -0,0 +1,46 @@
+oneVScrList <- function (base, data){
+
+    lists <- list()
+
+    colNames <- colnames(data)
+    sortData <- function(colNum){
+        for(i in 1:length(lists)){
+            if(i == colNum){
+                writeList(lists[[i]], sort(data[,i]), clear = TRUE)
+            }else{
+                writeList(lists[[i]], data[match(sort(data[,colNum]),
+                              data[,colNum]), i], clear = TRUE)
+            }
+        }
+    }
+    for (i in 1:length(colNames)) {
+        tempFrame <- tkframe(base)
+        tkpack(tkbutton(tempFrame, text = colNames[i],
+                        width = nchar(colNames[i])), expand = FALSE, fill = "x")
+        lists[[colNames[i]]] <- tklistbox(tempFrame,
+                                     width = max(sapply(data[,i], nchar)))
+        writeList(lists[[colNames[i]]], data[,i])
+        tkpack(lists[[colNames[i]]], expand = TRUE, fill = "both")
+        fun <- function() {}
+        body <- list(as.name("{"),
+                     substitute(sortData(j), list(j = i)))
+        body(fun) <- as.call(body)
+        tempBut <- tkbutton(tempFrame, text = "Sort", width = 5,
+                            command = fun)
+        tkpack(tempBut, expand = FALSE, fill = "x")
+        tkpack(tempFrame, side = "left", expand = FALSE, fill = "y")
+    }
+    bindYView <- function(...){
+        for(i in lists){
+            tkyview(i,...)
+        }
+    }
+
+    vScr <- tkscrollbar(base, orient = "vertical", command = bindYView)
+    tkpack(vScr, side = "right", fill = "y")
+    for (i in lists) {
+        tkconfigure(i, yscrollcommand = function(...) tkset(vScr,
+            ...))
+    }
+    return(lists)
+}
