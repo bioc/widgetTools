a40fbdef13a59796f45685eb957968d39511e2ca
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2004-05-04 20:52:28 +0000

    fixes to safeFileOpen.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@8493 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/safeFileOpen.R
+++ R/safeFileOpen.R
@@ -3,9 +3,32 @@
 
 safeFileOpen <- function(fileName) {
 
-    if (file.exists(fileName))
-        file(fileName)
-    else
-        paste(fileName,"doest not exist")
+    if (file.exists(fileName)){
+        info <- file.info(fileName)
+        if(info$isdir){
+            return(paste(fileName, "is a directory"))
+        }
+        if(file.access(fileName, mode = 4) != 0){
+            return(paste("You don't have read permission to file", fileName))
+        }
+        if(regexpr("\\.rda$|\\.Rda$", fileName) > 0){
+            if(readLines(fileName, n = 1) == "RDX2"){
+                return(paste(fileName, "is a binary file"))
+            }else{
+                return(file(fileName))
+            }
+        }
+        if(regexpr("\\.gz", fileName) > 0){
+            return(gzfile(fileName))
+        }else if(regexpr("\\.zip", fileName) > 0){
+            return(unz(fileName))
+        }else if(regexpr("\\.bz2", fileName) > 0 ){
+            return(bzfile(fileName))
+        }else{
+            return(file(fileName))
+        }
+    }else
+        return(paste(fileName,"doest not exist"))
 }
+
 
