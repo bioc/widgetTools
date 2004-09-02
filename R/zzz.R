75d1ba60b13d40e23580f1a8a4eccdf996c1ead0
unknown <unknown@email>
2004-09-02 18:10:19 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_1_4_branch'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_4_branch/madman/Rpacks/widgetTools@9859 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,30 @@
+# This function loads the required libraries and initializes the classes
+
+.First.lib <- function(libname, pkgname, where) {
+    capable <- capabilities()
+    if(!capable["tcltk"]){
+        stop(paste("The tcl/tk library is not available in your system.",
+                   "Download/install the tcltk library from",
+                   "www.tcl.tk/software/tcltk/"))
+    }else{
+        if(interactive()){
+            out <- paste("Package tcltk not able to be loaded!")
+            if (.Platform$OS.type == "windows")
+                out <- paste(out,"\nThe most likely cause of this",
+                             "is that your Tcl/Tk installation is",
+                             "misconfigured\nPlease see the R",
+                             "Windows FAQ, question 3.6:\n",
+                             "http://cran.r-project.org/bin/windows/contrib/rw-FAQ.html#Package%20TclTk%20does%20not%20work.")
+
+            require("tcltk", character.only = TRUE) || stop(out)
+        }
+    }
+
+ if((.Platform$OS.type == "windows") && ("Biobase" %in% installed.packages()[,"Package"])
+    && (interactive()) && (.Platform$GUI ==  "Rgui")){
+     if (require("Biobase"))
+         addVigs2WinMenu("widgetTools")
+ }
+
+
+}
