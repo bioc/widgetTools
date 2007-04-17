f18266d2884f1f0200899e1d436f50a359fac730
Seth Falcon <sfalcon@fhcrc.org>
2007-04-17 17:03:59 +0000

    Cleanup of widgetTools
    This resolves a warning on Windows along with various cleanups.
    The package now simply depends on tcltk.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/widgetTools@23960 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,32 +1,14 @@
-# This function loads the required libraries and initializes the classes
-
 .onLoad <- function(libname, pkgname, where) {
+    require("methods", quietly=TRUE) || stop("Package methods not available")
     capable <- capabilities()
-    if(!capable["tcltk"]){
-        stop(paste("The tcl/tk library is not available in your system.",
-                   "Download/install the tcltk library from",
-                   "www.tcl.tk/software/tcltk/"))
-    }else{
-        if(interactive()){
-            out <- paste("Package tcltk not able to be loaded!")
-            if (.Platform$OS.type == "windows")
-                out <- paste(out,"\nThe most likely cause of this",
-                             "is that your Tcl/Tk installation is",
-                             "misconfigured\nPlease see the R",
-                             "Windows FAQ, question 3.6:\n",
-                             "http://cran.r-project.org/bin/windows/contrib/rw-FAQ.html#Package%20TclTk%20does%20not%20work.")
-
-            require("tcltk", character.only = TRUE) || stop(out)
-        }
+    if (!capable["tcltk"]) {
+        stop("The tcltk package is not working.  Please consult the R FAQ")
     }
-
-    require("methods", character.only = TRUE, quietly = TRUE) ||
-    stop("Package methods not available")
-    require("tcltk", quietly = TRUE) || stop("Package tcltk unavailalbe!")
-
- if((.Platform$OS.type == "windows") && ("Biobase" %in% installed.packages()[,"Package"])
-    && (interactive()) && (.Platform$GUI ==  "Rgui")){
-     if (require("Biobase"))
-         addVigs2WinMenu("widgetTools")
- }
 }
+
+.onAttach <- function(libname, pkgname) {
+    if (.Platform$OS.type == "windows")
+      suppressWarnings(require("Biobase")) && addVigs2WinMenu("widgetTools")
+}
+
+
